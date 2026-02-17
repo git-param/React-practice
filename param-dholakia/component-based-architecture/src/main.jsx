7b68fca013647b12a540aef069b0f8d4cc0c612f
git-param <githubparam0@gmail.com>
2026-02-17 12:52:32 +0530

    Implemented Component based architecture

--- param-dholakia/component-based-architecture/src/main.jsx
+++ param-dholakia/component-based-architecture/src/main.jsx
@@ -3,9 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('param')).render(
-  // <StrictMode>
-  //   <App />
-  // </StrictMode>,
-  <h1>Param</h1>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
