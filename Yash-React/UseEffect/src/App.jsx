2c1c4797053ec89c64e0e8edd29f69bfd0dc6aaf
Yashp07 <yashpatel.inexture@gmail.com>
2026-02-24 11:32:43 +0530

    adkbsh

--- Yash-React/UseEffect/src/App.jsx
+++ Yash-React/UseEffect/src/App.jsx
@@ -1,23 +1,10 @@
 import React from 'react'
 
-import EffectBasic from './components/EffecBasicDemo'
-import FirstMountExample from './components/FirstMountDemo'
-import UnmountExample from './components/Cleanup'
-import UpdateExample from './components/DidUpdateDemo'
 import CounterWithLifecycle from './components/Example'
 
 function App(){
   return (
     <div>
-      {/* <EffectBasic/>
-      <hr /> */}
-      {/* <FirstMountExample /> */}
-      <hr />
-      {/* <UpdateExample />   */}
-      <hr />
-      {/* <UnmountExample />
-       */}
-       <hr />
        <CounterWithLifecycle />
     </div>
   ) 
